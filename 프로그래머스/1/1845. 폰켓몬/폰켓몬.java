469185cf12694835ce0acf94436903dbb2878a99
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-09-24 01:49:51 +0900

    [level 1] Title: 폰켓몬, Time: 7.24 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new TreeSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        if (set.size() <= nums.length / 2) {
+            return set.size();
+        }
+        
+        return nums.length / 2;
+    }
+}
