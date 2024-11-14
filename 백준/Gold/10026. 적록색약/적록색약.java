af207820ee2c007fa2b7e015aae25a2c9a317541
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-11-15 03:42:47 +0900

    [Gold V] Title: 적록색약, Time: 112 ms, Memory: 15048 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     static int n;
@@ -8,33 +7,33 @@ public class Main {
     static int[] mrow = {0, 0, 1, -1};
     static int[] mcol = {1, -1, 0, 0};
     static int cnt;
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
+
         n = Integer.parseInt(br.readLine());
         arr = new char[n][n];
         visited = new boolean[n][n];
-        
+
         for (int i = 0; i < n; i++) {
             char[] crr = br.readLine().toCharArray();
             for (int j = 0; j < n; j++) {
                 arr[i][j] = crr[j];
             }
         }
-        
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j]) {
-                    bfs(i, j);
+                    dfs(i, j);
                     cnt++;
                 }
             }
         }
-        
+
         sb.append(cnt).append(" ");
-        
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (arr[i][j] == 'G') {
@@ -42,56 +41,37 @@ public class Main {
                 }
             }
         }
-        
+
         cnt = 0;
         visited = new boolean[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j]) {
-                    bfs(i, j);
+                    dfs(i, j);
                     cnt++;
                 }
             }
         }
-        
+
         sb.append(cnt);
-        
+
         System.out.println(sb);
     }
-    
-    private static void bfs(int row, int col) {
-        char color = arr[row][col];
+
+    private static void dfs(int row, int col) {
         visited[row][col] = true;
-        
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(row, col));
-        
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            
-            for (int i = 0; i < 4; i++) {
-                int nextRow = cur.row + mrow[i];
-                int nextCol = cur.col + mcol[i];
-                
-                if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n) {
-                    continue;
-                }
-                
-                if (!visited[nextRow][nextCol] && arr[nextRow][nextCol] == color) {
-                    visited[nextRow][nextCol] = true;
-                    q.add(new Node(nextRow, nextCol));
-                }
+
+        for (int i = 0; i < 4; i++) {
+            int nextRow = row + mrow[i];
+            int nextCol = col + mcol[i];
+
+            if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n) {
+                continue;
             }
-        }
-    }
-    
-    static class Node {
-        int row;
-        int col;
-        
-        public Node(int row, int col) {
-            this.row = row;
-            this.col = col;
+
+            if (!visited[nextRow][nextCol] && arr[nextRow][nextCol] == arr[row][col]) {
+                dfs(nextRow, nextCol);
+            }
         }
     }
 }
