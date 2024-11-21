00230d5bfdd84771a8928de78e04e70cb435e33f
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-11-22 03:49:47 +0900

    [Gold IV] Title: 연구소, Time: 388 ms, Memory: 144964 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -3,104 +3,112 @@ import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[][] arr;
-    static int[][] virusArr;
+    static int[][] map;
+    static List<Node> wallPosition = new ArrayList<>();
+    static int[] wallArr;
+    static boolean[][] virusVisited;
+    static int[][] virusMap;
     static int[] mrow = {0, 0, 1, -1};
     static int[] mcol = {1, -1, 0, 0};
-    static int max;
-    
+    static int max = Integer.MIN_VALUE;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        
-        arr = new int[n][m];
+
+        map = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        dfs(0);
-        
-        System.out.println(max);
-    }
-    
-    private static void dfs(int wallCnt) {
-        if (wallCnt == 3) {
-            bfs();
-            return;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
-                    arr[i][j] = 1;
-                    dfs(wallCnt + 1);
-                    arr[i][j] = 0;
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 0) {
+                    wallPosition.add(new Node(i, j));
                 }
             }
         }
+
+        wallArr = new int[3];
+        dfs(0, 0);
+
+        System.out.println(max);
     }
-    
-    private static void bfs() {
-        virusArr = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            virusArr[i] = arr[i].clone();
+
+    private static void dfs(int depth, int start) {
+        if (depth == 3) {
+            bfs();
+            return;
         }
-        
+
+        for (int i = start; i < wallPosition.size(); i++) {
+            wallArr[depth] = i;
+            dfs(depth + 1, i + 1);
+        }
+    }
+
+    private static void bfs() {
         Queue<Node> q = new LinkedList<>();
-        
+
+        virusVisited = new boolean[n][m];
+        virusMap = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (virusArr[i][j] == 2) {
+                virusMap[i][j] = map[i][j];
+                if (virusMap[i][j] == 2) {
                     q.add(new Node(i, j));
                 }
             }
         }
-        
+
+        for (int i = 0; i < wallArr.length; i++) {
+            int row = wallPosition.get(wallArr[i]).row;
+            int col = wallPosition.get(wallArr[i]).col;
+            virusVisited[row][col] = true;
+            virusMap[row][col] = 1;
+        }
+
         while (!q.isEmpty()) {
             Node cur = q.poll();
-            
+
             for (int i = 0; i < 4; i++) {
                 int nextRow = cur.row + mrow[i];
                 int nextCol = cur.col + mcol[i];
-                
+
                 if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= m) {
                     continue;
                 }
-                
-                if (virusArr[nextRow][nextCol] == 0) {
-                    virusArr[nextRow][nextCol] = 2;
+
+                if (!virusVisited[nextRow][nextCol] && virusMap[nextRow][nextCol] == 0) {
+                    virusVisited[nextRow][nextCol] = true;
+                    virusMap[nextRow][nextCol] = 2;
                     q.add(new Node(nextRow, nextCol));
                 }
             }
         }
-        
-        max = Math.max(max, cntSafeZone());
+
+        max = Math.max(cntSafeZone(), max);
     }
-    
+
     private static int cntSafeZone() {
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (virusArr[i][j] == 0) {
+                if (virusMap[i][j] == 0) {
                     cnt++;
                 }
             }
         }
-        
+
         return cnt;
     }
-    
+
     static class Node {
         int row;
         int col;
-        
+
         public Node(int row, int col) {
             this.row = row;
             this.col = col;
