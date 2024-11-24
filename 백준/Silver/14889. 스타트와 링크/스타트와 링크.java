0627858cf49a5ffbd8fcf1597a66b8668e0e52a4
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-11-24 17:33:25 +0900

    [Silver I] Title: 스타트와 링크, Time: 348 ms, Memory: 19256 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static boolean[] visited;
+    static int[][] map;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        visited = new boolean[n];
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0, 0);
+
+        System.out.println(min);
+    }
+
+    private static void dfs(int depth, int at) {
+        if (depth == n / 2 && visited[0]) {
+            sum();
+            return;
+        }
+
+        for (int i = at; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(depth + 1, i + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private static void sum() {
+        int start = 0;
+        int link = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (visited[i] && visited[j]) {
+                    start += map[i][j];
+                    start += map[j][i];
+                } else if (!visited[i] && !visited[j]) {
+                    link += map[i][j];
+                    link += map[j][i];
+                }
+            }
+        }
+
+        min = Math.min(min, Math.abs(start - link));
+    }
+}
